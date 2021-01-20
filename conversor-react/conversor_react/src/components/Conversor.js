099e31d7f71642aeb532d30393df830c450c3801
bucard <rafaelbucard@gmail.com>
2021-01-20 19:00:50 -0300

    Component conversor

---
+++ conversor-react/conversor_react/src/components/Conversor.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Conversor extends Component {
+    render() {
+        return (
+            <div className="conversor">
+                <p>Olá mundo!!</p>
+            </div>
+        );
+    }
+}
