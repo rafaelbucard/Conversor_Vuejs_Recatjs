43b933e1ae55ca2878c0ead372c4a0026aedb51b
bucard <rafaelbucard@gmail.com>
2021-01-22 02:30:28 -0300

    flex box

--- conversor-react/conversor_react/src/App.js
+++ conversor-react/conversor_react/src/App.js
@@ -4,7 +4,19 @@ import Conversor from './components/Conversor';
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <div className="linha">
+          <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+          <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+      <div className="linha">
+          <Conversor moedaA="ARS" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="ARS"></Conversor>
+      </div>
+      
     </div>
   );
 }
--- conversor-react/conversor_react/src/components/Conversor.js
+++ conversor-react/conversor_react/src/components/Conversor.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './Conversor.css';
 export default class Conversor extends Component {
 
     constructor(props) {
@@ -30,7 +30,7 @@ this.setState({moedaB_valor})
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
                 <input type="text" onChange= {(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
                 <button type="button" value="Converter" onClick={this.converter}>Converter</button>
-                <h2>{this.state.moedaB_valor}</h2>
+                <h2>{this.props.moedaB} {this.state.moedaB_valor}</h2>
             </div>
         );
     }
